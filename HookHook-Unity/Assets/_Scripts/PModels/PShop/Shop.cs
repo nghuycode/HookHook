d0a2bebc3a77cdd8f8dc31c82bc88d2f196ea4c3
tthienphuc <44915899+tthienphuc@users.noreply.github.com>
2020-08-11 23:05:20 +0700

    PModels
    User + Shop

---
+++ HookHook-Unity/Assets/_Scripts/PModels/PShop/Shop.cs
@@ -0,0 +1,32 @@
+﻿using PHelper;
+using PItem;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PShop
+{
+    public class Shop
+    {
+        public List<Item> Items = new List<Item>();
+        public Shop()
+        {
+            string tmp = SaveLoadHelper.Load("/shop");
+            if (tmp != null)
+                Items = JsonUtility.FromJson<ShopSaveModel>(tmp).items;
+            else
+                Save();
+        }
+
+        public void addItem(Item item)
+        {
+            Items.Add(item);
+            Save();
+        }
+
+        private void Save()
+        {
+            SaveLoadHelper.Save("/shop", new ShopSaveModel() { items = Items });
+        }
+    }
+}
---
+++ HookHook-Unity/Assets/_Scripts/PModels/PUser/User.cs
@@ -0,0 +1,41 @@
+using PHelper;
+using PItem;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PUser
+{
+    [Serializable]
+    public class User
+    {
+        public string Name = "";
+        public int Money = 0;
+        public List<Item> Purchased = new List<Item>();
+        public User()
+        {
+            string data = SaveLoadHelper.Load("/user");
+            if (data != null)
+            {
+                UserSaveModel tmp = JsonUtility.FromJson<UserSaveModel>(data);
+                this.Name = tmp.Name;
+                this.Money = tmp.Money;
+                this.Purchased = tmp.Purchased;
+            }
+            else
+                Save();
+        }
+
+        public void NewUser(string name)
+        {
+            this.Name = name;
+            Save();
+        }
+
+        private void Save()
+        {
+            SaveLoadHelper.Save("/user", this);
+        }
+    }
+}
