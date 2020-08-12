b36e3f7f111e81073508f5183753fe3bc01690f2
nghuycode <66381199+nghuycode@users.noreply.github.com>
2020-08-12 11:26:51 +0700

    Prototype GameManager

---
+++ HookHook-Unity/Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public bool CanPlay;
+    public bool IsWin;
+    public int CurrentLevel;
+
+    #region Mono Behaviour
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    #region Events and Actions
+    public event Action OnInitGame;
+    public event Action OnStartGame;
+    public event Action OnPauseGame;
+    public event Action OnWinGame;
+    public event Action OnLoseGame;
+
+    public event Action OnUpdateProgressLevel;
+    public event Action OnUpdateGem;
+    public void InitGame()
+    {
+        if (OnInitGame != null)
+            OnInitGame();
+    }
+    public void StartGame()
+    {
+        if (OnStartGame != null)
+            OnStartGame();
+    }
+    public void PauseGame()
+    {
+        if (OnPauseGame != null)
+            OnPauseGame();
+    }
+    public void WinGame()
+    {
+        if (OnWinGame != null)
+            OnWinGame();
+    }
+    public void LoseGame()
+    {
+        if (OnLoseGame != null)
+            OnLoseGame();
+    }
+
+    public void UpdateProgressLevel()
+    {
+        if (OnUpdateProgressLevel != null)
+            OnUpdateProgressLevel();
+    }
+    public void UpdateGem()
+    {
+        if (OnUpdateGem != null)
+            OnUpdateGem();
+    }
+    #endregion
+}
