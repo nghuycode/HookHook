edeef15f20f38d29b0d77e30dd1eb3b547a5a66c
nghuycode <66381199+nghuycode@users.noreply.github.com>
2020-08-16 09:57:40 +0700

    Checkpoint dev-ngh + dev-nhatlong

--- HookHook-Unity/Assets/_Scripts/Managers/InGameSceneManager.cs
+++ HookHook-Unity/Assets/_Scripts/Managers/InGameSceneManager.cs
@@ -4,12 +4,23 @@ public class InGameSceneManager : MonoBehaviour
 {
     public GameObject[] LevelPrefabList;
 
-    private void Start() 
+    [SerializeField]
+    private Vector3 playerDefaultPosition;
+    private GameObject Player;
+
+    private void Awake() 
     {
+        Player = GameObject.Find("Player");
         RenderLevel(0);    
     }
     public void RenderLevel(int id)
     {
+        //Instantiate level
         GameObject level = Instantiate(LevelPrefabList[id],Vector3.zero,Quaternion.identity);
+        level.transform.SetParent(this.transform);
+
+        //Show player and Reset Player position
+        Player.GetComponent<SpriteRenderer>().enabled = true;
+        Player.transform.position = playerDefaultPosition;
     }
 }
