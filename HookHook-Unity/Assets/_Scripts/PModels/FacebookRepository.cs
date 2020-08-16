784dbe57bcb12940ff24134e3668e480115e84ee
LeeHoanMinh <Leehoanminh@gmail.com>
2020-08-16 15:43:47 +0700

    Revert "Merge With Phuc"
    This reverts commit 5b62af2fb1850e4503c9ecc36722f8501ec24870.

---
+++ HookHook-Unity/Assets/_Scripts/PModels/FacebookRepository.cs
@@ -0,0 +1,17 @@
+﻿using PFacebook;
+
+namespace PModels
+{
+    public class FacebookRepository
+    {
+        static private MyFacebook myFacebook;
+        public static MyFacebook MyFacebook
+        {
+            get {
+                if (myFacebook == null)
+                    myFacebook = new MyFacebook();
+                return myFacebook;
+            }
+        }
+    }
+}
