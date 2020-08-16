39b0fbed0286322e78d201f283743fd51de00fd0
nhatlongcode <nhatlongcode@gmail.com>
2020-08-16 09:27:49 +0700

    add effect to some obstacle

---
+++ HookHook-Unity/Assets/Animation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    //adjust this to change speed
+    [SerializeField]
+    float speed = 5f;
+    //adjust this to change how high it goes
+    [SerializeField]
+    float height = 0.5f;
+
+    Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+    void Update()
+    {
+
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z) ;
+    }
+}
