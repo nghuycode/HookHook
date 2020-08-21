ef2e3de962dbd3a97ec21795ed8a5513f69afdfa
nghuycode <66381199+nghuycode@users.noreply.github.com>
2020-08-21 20:12:14 +0700

    Merge remote-tracking branch 'origin/Dev-Clacck' into dev-ngh
    # Conflicts:
    #	HookHook-Unity/Assets/Plugins/Android.meta
    #	HookHook-Unity/Assets/_Resources/Music.meta
    #	HookHook-Unity/Assets/_Resources/in game UI/Ingameui.png
    #	HookHook-Unity/Assets/_Resources/shop/bgbasic.png.meta
    #	HookHook-Unity/Assets/_Scenes/DemoHook.unity
    #	HookHook-Unity/Assets/_Scripts/Isolated/InGameUIManager.cs
    #	HookHook-Unity/Assets/_Scripts/Isolated/SkinAdapt.cs
    #	HookHook-Unity/Assets/_Scripts/Managers/GameManager.cs
    #	HookHook-Unity/Assets/_Scripts/PModels/PModels.meta
    #	HookHook-Unity/Assets/_Scripts/Scenes/ShopMenu/ShopMoneyBar.cs

---
+++ HookHook-Unity/Assets/_Scripts/Scenes/ShopMenu/ShopPanelUser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PItem;
+using UnityEngine.UI;
+
+public class ShopPanelUser : MonoBehaviour
+{
+    static public ShopPanelUser SPU;
+    public Image userImg;
+
+    void Awake()
+    {
+        if (SPU != null)
+            GameObject.Destroy(SPU);
+        else
+            SPU = this;
+       
+    }
+    public void UpdateImg(Image img)
+    {
+        userImg.sprite = img.sprite;        
+    }
+}
