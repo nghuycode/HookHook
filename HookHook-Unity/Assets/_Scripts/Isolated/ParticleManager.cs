c728c978b8754f8c4cc8199656c4e97d3576c7c3
nghuycode <66381199+nghuycode@users.noreply.github.com>
2020-08-22 12:22:04 +0700

    + Particle Manager

---
+++ HookHook-Unity/Assets/_Scripts/Isolated/ParticleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    public GameObject[] Effects;
+    private void Start() {
+        GameManager.Instance.OnStartGame += GenerateEffects;
+    }
+    private void GenerateEffects() {
+        int numberOfEffect = Random.Range(1, 3);
+        int effectIndex = -1, tmp = -1;
+        for (int i = 0; i < numberOfEffect; ++i) {
+            do 
+            {
+                tmp = Random.Range(0, Effects.Length);
+            }
+            while (tmp == effectIndex);
+            effectIndex = tmp;
+            Effects[effectIndex].SetActive(true);
+        }
+    }
+}
