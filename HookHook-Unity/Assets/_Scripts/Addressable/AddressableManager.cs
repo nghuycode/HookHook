d70be423f36e6b453782102e6cc9a196c3aa4810
nghuycode <66381199+nghuycode@users.noreply.github.com>
2021-06-16 15:34:10 +0700

    INIT

---
+++ HookHook-Unity/Assets/_Scripts/Addressable/AddressableManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations; 
+
+public class AddressableManager : MonoBehaviour
+{
+    public AssetReference HookHookScene;
+    public void LoadHookHookSceneAsync() 
+    {
+        HookHookScene.LoadSceneAsync(UnityEngine.SceneManagement.LoadSceneMode.Additive);
+    }
+}
